ae7e7d3ab501b1f158bd7a32da46b0df95921887
giovanna-clc <giovanna.couto@sempreceub.com>
2024-08-27 10:48:40 -0300

    Alteraações JS

--- JS/index.js
+++ JS/index.js
@@ -2,10 +2,34 @@ const diaSemana = document.getElementById("dia-semana");
 const diaMesAno = document.getElementById("dia-mes-ano");
 const horaMinSeg = document.getElementById("hora-min-seg");
 
-diaSemana.textContent = "Domingoooooooooo";
+diaSemana.textContent = getWeekDay();
 diaMesAno.textContent = getCurrentDate();
 
+function getWeekDay() {
+    const date = new date ();
+    let days = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+    return days [DataTransfer, getDay ()];
+}
+
+
+function getCurrentHour() {
+
+}
+
 function getCurrentDate() {
+    // Alterar a solucao para considerar padStart ou slice
+    // Considerar formatos diferentes de data, conforme a localização
+    // do usuário dd/mm/aa
     const date = new Date();
     return date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear();
 }
+
+function printCurrentHour() {
+    horaMinSeg.textContent = getCurrentHour();
+}
+
+function register() {
+    alert("Bater ponto!");
+}
+
+setInterval(printCurrentHour, 1000);
