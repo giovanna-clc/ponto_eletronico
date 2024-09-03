3dd70242a3bbc7457942112e4de0d900895abf82
giovanna-clc <giovanna.couto@sempreceub.com>
2024-09-03 08:55:49 -0300

    Alterações com base na última aula

--- JS/index.js
+++ JS/index.js
@@ -2,26 +2,44 @@ const diaSemana = document.getElementById("dia-semana");
 const diaMesAno = document.getElementById("dia-mes-ano");
 const horaMinSeg = document.getElementById("hora-min-seg");
 
+const btnBaterPonto = document.getElementById("btn-bater-ponto");
+btnBaterPonto.addEventListener("click", register);
+
 diaSemana.textContent = getWeekDay();
 diaMesAno.textContent = getCurrentDate();
 
 function getWeekDay() {
-    const date = new date ();
+    const date = new Date();
     let days = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
-    return days [DataTransfer, getDay ()];
+    return days[date.getDay()];
 }
-
 
 function getCurrentHour() {
-
+    // Considerar os métodos abaixo para incluir zeros em numeros < 10
+    // padStart()
+    // slice()
+    // formatos de hora considerando o locale do usuário
+    const date = new Date();
+    return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 }
 
+
 function getCurrentDate() {
-    // Alterar a solucao para considerar padStart ou slice
-    // Considerar formatos diferentes de data, conforme a localização
-    // do usuário dd/mm/aa
+    // Alterar a solução para considerar padStart ou slice
+    // Considerar formatos diferentes da data, conforme localização
+    // do usuário dd/mm/aaaa, mm/dd/aaaa, aaaa/mm/dd, aaaa.mm.dd
+    // Verificar se no Date() há algum método que possa auxiliar
+    // locale
     const date = new Date();
-    return date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear();
+    let month = date.getMonth();
+    let day = date.getDate();
+    if (day < 10) {
+        day = "0" + day
+    }
+    if (month < 10) {
+        month = "0" + (month + 1)
+    }
+    return day + "/" + month + "/" + date.getFullYear();
 }
 
 function printCurrentHour() {
@@ -29,7 +47,9 @@ function printCurrentHour() {
 }
 
 function register() {
-    alert("Bater ponto!");
+    // Abrir <dialog> com, no mínimo, 4 botões
+
+    alert("Bater Ponto!");
 }
 
 setInterval(printCurrentHour, 1000);
